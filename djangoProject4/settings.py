c97857ea2874fd9bf7f722f464311b3fa75b6bef
Aayush Kumar Singh <malemeaayushkumarsingh@gmail.com>
2022-01-11 11:55:53 +0530

    Update settings.py

--- djangoProject4/settings.py
+++ djangoProject4/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "mynotes/build/static")
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
